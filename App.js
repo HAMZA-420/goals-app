13229e3a0514ca01d392f7c55dd7f6bca83ff2ba
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-01-18 12:43:22 +0500

    deleting items

--- App.js
+++ App.js
@@ -13,6 +13,11 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(),value: goalTitle}]);
   };
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    })
+  }
 
   return (
     <View style={styles.screen}>
@@ -20,7 +25,7 @@ export default function App() {
     <FlatList 
     keyExtractor={(item, index)=>item.id} 
     data ={courseGoals}
-    renderItem = {itemData => <GoalItem onDelete={()=> console.log("Yes its Working")} title={itemData.item.value} />}
+    renderItem = {itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
     />
     </View>
   );
